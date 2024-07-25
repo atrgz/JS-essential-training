e8efed5399d88dc71b9c1cb2ace44c76366a30ad
Toni <atorresrguez@gmail.com>
2024-07-25 11:40:58 +0200

    "Practice 0307"

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,16 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bottle = {
+  capacity: 1000,
+  material: "glass",
+  lidMaterial: "aluminium",
+  content: {
+    state: "liquid",
+    type: "water",
+    quatity: 200,
+  },
+};
+
+console.log("The bottle object:", bottle);
